cc492a2e449b51e1d18cb4ed2bfaa1f710524585
Michelle Mercer <yuming.michelle@gmail.com>
2025-07-04 23:12:59 -0400

    fix migration issues

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,28 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Profile, CustomUser
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = [
-        'email',
-        'first_name',
-        'last_name',
-        'is_staff',
-        'is_active'
-    ]
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
 
-admin.site.register(CustomUser, CustomUserAdmin)
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,36 +1,54 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils import timezone
 
+class CustomUserManager(BaseUserManager):
 
-class CustomUserManager(UserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError('The given email must be set')
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Superuser must have is_staff=True.')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    position = models.CharField(max_length=100, null=True, blank=True)
-    education = models.CharField(max_length=100, null=True, blank=True)
-    biography = models.TextField(null=True, blank=True)
-    research_interests = ArrayField(models.CharField(max_length=100), null=True, blank=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     date_joined = models.DateTimeField(default=timezone.now)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = CustomUserManager()
 
-    def __str__(self):
-        return self.email
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    education = models.CharField(max_length=100)
+    biography = models.TextField()
+    research_interests = ArrayField(models.CharField(max_length=100))
