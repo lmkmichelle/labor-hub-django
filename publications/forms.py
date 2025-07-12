36785f1c25c88e615cfdcf5fdecdd76c77d52a26
Michelle Mercer <yuming.michelle@gmail.com>
2025-07-12 12:55:58 -0400

    working upload form stylized WIP keywords

--- publications/forms.py
+++ publications/forms.py
@@ -1,19 +1,33 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 from django import forms
+
+import publications
 from .models import Publication
-import datetime
 
 
 class PublicationForm(forms.ModelForm):
+    class Meta:
+        model = Publication
+        fields = ['title','month','year','abstract','country','keywords','study_url','is_job_market','pdf']
+
     authors_input = forms.CharField(
         required=False,
         label='Authors',
         widget=forms.TextInput(attrs={'id': 'authors-input'}),
     )
 
-    class Meta:
-        model = Publication
-        fields = [
-            'title',
+    keywords_input = forms.CharField(
+        required=False,
+        label='Keywords',
+        widget=forms.TextInput(attrs={'id': 'keywords-input'}),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout('title','authors_input',
             'month',
             'year',
             'abstract',
@@ -22,4 +36,7 @@ class PublicationForm(forms.ModelForm):
             'study_url',
             'is_job_market',
             'pdf'
-        ]
+        )
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+
---
+++ static/js/authors.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.querySelector("#authors-input");
+  const tagify = new Tagify(input, {
+    whitelist: [],
+    enforceWhitelist: false,
+    dropdown: {
+      enabled: 1,
+      maxItems: 10,
+      position: 'auto',
+      classname: 'users-list'
+    }
+  });
+
+  tagify.on('input', function (e) {
+    const value = e.detail.value;
+    fetch(`/api/accounts/search/?q=${encodeURIComponent(value)}`)
+      .then(res => res.json())
+      .then(data => {
+        tagify.settings.whitelist = data;
+        tagify.dropdown.show.call(tagify, value);
+      });
+  });
+});
