7a2d27701b1a4990dc4380b71119c9c1de671f6d
Michelle Mercer <yuming.michelle@gmail.com>
2025-09-30 02:08:29 -0400

    clean up js and added categories

--- static/js/tagify.js
+++ static/js/tagify.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", async function () {
-  const input = document.querySelector("#keywords-input, #research-interests-input");
+  const authors_input = document.querySelector("#authors-input");
+  const keywords_input = document.querySelector("#keywords-input");
+  const research_interests_input = document.querySelector("#research-interests-input");
+  const topic_input = document.querySelector("#topic-input");
   const additional_keywords = [
     "Education and Human Capital",
     "Labor Supply",
@@ -33,13 +36,43 @@ document.addEventListener("DOMContentLoaded", async function () {
     "Other"
   ];
 
+  const authors_tag = new Tagify(authors_input, {
+    whitelist: [],
+    enforceWhitelist: false,
+    dropdown: {
+      closeOnSelect: false,
+      enabled: 0,
+      maxItems: 10,
+    }
+  });
 
-  new Tagify(input, {
+  authors_tag.on('input', function (e) {
+    const value = e.detail.value;
+    fetch(`/api/accounts/search/?q=${encodeURIComponent(value)}`)
+      .then(res => res.json())
+      .then(data => {
+        authors_tag.settings.whitelist = data;
+      });
+  });
+
+  const topic_tag = new Tagify(topic_input);
+  const research_interests_tag = new Tagify(research_interests_input, {
     whitelist: additional_keywords,
     dropdown: {
       enabled: 0,
       closeOnSelect: false,
-      maxItems: 5,
+      maxItems: 10,
+      scroll
+    }
+  });
+
+  const keywords_tag = new Tagify(keywords_input, {
+    whitelist: additional_keywords,
+    dropdown: {
+      enabled: 0,
+      closeOnSelect: false,
+      maxItems: 10,
+      classnames: "tags__dropdown"
     },
 
     originalInputValueFormat: values =>
