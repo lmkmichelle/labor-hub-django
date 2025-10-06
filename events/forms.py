d03d6205a3fd0b2783788b994db1ef5829116016
Michelle Mercer <yuming.michelle@gmail.com>
2025-10-06 15:29:17 -0400

    events final touch

--- events/forms.py
+++ events/forms.py
@@ -1,4 +1,8 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, ButtonHolder, HTML
 from django import forms
+from django.urls import reverse_lazy
+
 from .models import Event
 
 
@@ -6,6 +10,8 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'description', 'date', 'end_date', 'location', 'category']
+
+
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Event Title'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Event Description'}),
@@ -18,4 +24,24 @@ class EventForm(forms.ModelForm):
             'date': 'Start Date & Time',
             'end_date': 'End Date & Time (Optional)',
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        cancel_url = reverse_lazy('events-list')
+
+        self.fields['end_date'].required = False
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            'title',
+            'description',
+            'date',
+            'end_date',
+            'location',
+            'category',
+            ButtonHolder(
+                Submit('submit', 'Submit', css_class='btn btn-primary me-3'),
+                HTML(f'<a href="{cancel_url}" style="margin-bottom: 0" class="btn btn-secondary">Cancel</a>')
+            )
+        )
 
--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from events.views import EventsListView, EventCreateView
+from events.views import EventsListView, EventCreateView, EventsDetailView
 
 urlpatterns = [
     path('', EventsListView.as_view(), name='events-list'),
     path('create/', EventCreateView.as_view(), name='event-create'),
+    path('<int:pk>/', EventsDetailView.as_view(), name='event-detail'),
 ]
